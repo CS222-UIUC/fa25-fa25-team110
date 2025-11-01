39b1933cea265792bd7a54fe04894d4d46d36220
julia <jszen@illinois.edu>
2025-11-01 10:33:10 -0500

    Fix

--- classwork_chatbot/settings.py
+++ classwork_chatbot/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-axb*ut65(%5mx3xiks(oq=-2^*(abn5o(wq8r*-$wfk646v&2s"
+import os
+
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key-for-local-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
